17035464734913aaf487494ed8e2d9521e4ac41d
Gustavo Luszczynski <gustavo.duarte@redhat.com>
2014-10-21 15:31:08 -0200

    change loopback address

--- keycloak/runDocker.sh
+++ keycloak/runDocker.sh
@@ -6,7 +6,7 @@
 
 . ../docker.properties
 
-CMD_START="docker run --rm -p 127.0.0.1:8080:8080 -p 127.0.0.1:9990:9990 --privileged --name $KEYCLOAK_CONTAINER_NAME";
+CMD_START="docker run --rm -p 127.0.0.2:8080:8080 -p 127.0.0.2:9990:9990 --privileged --name $KEYCLOAK_CONTAINER_NAME";
 
 isApache=$(sudo docker ps | grep $APACHE_CONTAINER_NAME)
 test "x$isApache" = "x" || CMD_START="$CMD_START --link $APACHE_CONTAINER_NAME:$APACHE_CONTAINER_NAME"
